1f2029da56d23d194b9c43eb1e2b8a2b10a05047
Minhyuk Kim <kimminhyuk1004@gmail.com>
2023-02-28 15:46:33 +0900

    Remove unnecessary dependencies

--- QuickStart/AppDelegate.swift
+++ QuickStart/AppDelegate.swift
@@ -5,22 +5,6 @@
 //  Created by Minhyuk Kim on 2022/08/29.
 //
 
-// SBUView
-// SBULifeCycle
-// sbu_constraints
-
-// let a = SBUChannelViewController()
-// -> view model -> SendbirdChat / SendbirdUI reference
-
-// set current user
-// call SendbirdUI.connect
-
-// 화면 로딩할때마다 update user info (렌더링 하는 부분들만)
-// -> 유저가 다르면? -> 에러뱉고 끝
-
-// view model initialize할떄 SendbirdUI.connectIfNeeded -> current User 확인 & connect
-
-
 import UIKit
 
 @main
@@ -44,7 +28,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
-
